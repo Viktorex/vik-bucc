94248d7d9fad694185bcf686919294126d8a4b06
Viktor <144249434+Viktorex@users.noreply.github.com>
2024-03-24 01:20:29 +0100

    Add files via upload

---
+++ signup1.js
@@ -0,0 +1,28 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Simple validation (just checking if username and password are not empty)
+    if (username.trim() === "" || password.trim() === "") {
+        document.getElementById("error-message").textContent = "Username and password are required!";
+    } else {
+        // Here you can add your login logic (e.g., send login request to server)
+        // For demo purposes, just displaying the username and password
+        alert("Username: " + username + "\nPassword: " + password);
+        document.getElementById("error-message").textContent = "";
+    }
+});
+
+// test.addEventListener("mouseover", setDark);
+// test.addEventListener("mouseout", setLight);
+
+// function setDark() {
+//     document.body.style.background = "#154979";
+//     test.style.color = "white";
+// }
+
+// function setLight() {
+//     document.body.style.background = "white";
+//     test.style.color = "white"
+// }
